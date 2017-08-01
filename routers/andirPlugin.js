50f5f85ed5b614a96126076bce12f9f891846f9e
zhangchenze <zhangchenze@gomeholdings.com>
2017-08-01 18:31:50 +0800

    修改返回数组

--- routers/andirPlugin.js
+++ routers/andirPlugin.js
@@ -59,10 +59,32 @@ class andirApiController {
  * 
  * @param {*} data 数组
  * @param 返回类别最新的值
+ * @param 核心处理逻辑
  */
 
 const list = async (data) => {
-    return data.reduce(async (pre, cur) => await redis.lrange(cur, 0, -1), []);
+
+    /**
+     * @param data 数据库中插件详情详情存储字段的个数，也就是长度
+     * @param data的长度决定了要不对其中一个进行比较处理，获取最新的
+     */
+
+    if (data.length > 1) {
+
+        /**
+         * 核心处理逻辑
+         */
+
+        return (async () => {
+            const b = data.map(async v => await redis.lrange(v, 0, -1));
+            const c = await Promise.all(b);
+            return c;
+        })();
+
+    } else {
+        return data.reduce(async (pre, cur) => await redis.lrange(cur, 0, -1), []);
+    }
+
 };
 
 const objDate = async (ctx, data) => {
@@ -71,48 +93,80 @@ const objDate = async (ctx, data) => {
     const androidVer = ctx.query.systemVer;
     const channl = ctx.query.channl;
 
-    let d, d1, d2;
+    let d, d1, d2, arr = [];
 
     /**
-     * 这块非常复杂，由于redis库本身限制问题，特对数据进行遍历处理
-     * @param 对所有的数据进行json化处理
+     * 话说看到了这里，你就牛逼了。
+     * @param data里面有可能是但数组，也有可能是多数组
      */
 
-    let temp = data
-        .map(JSON.parse);
-    
-    /**
-     * 插件全部使用，数据
-     */    
-    
-    let temp3 = temp.filter(e => e.optionsRadios == '1');
+    let temp;
 
-    /**
-     * 插件条件使用，数据
-     */ 
-    
-    let temp2 = temp.filter(e => e.optionsRadios == '0' && (appVer == e.appVer || androidVer == e.systemVer || channl == e.channl));
+    for (var i = 0; i < data.length; i++) {
 
+        temp = '';
 
-    /**
-     * 以下为分别获取最大值
-     */
+        if (typeof data[i] == 'string') {
 
-    if (temp3.length > 0) {
-        d1 = temp3.map(e => (e.plugVersion = Number(e.plugVersion), e)).reduce((pre, cur) => pre.plugVersion > cur.plugVersion ? pre : cur);
+            /**
+             * 数组里面是单数组的时候
+             */
+
+            temp = data
+                .map(JSON.parse);
+        } else {
+            
+            /**
+             * 数组里面为多数组
+             */
+
+            temp = data[i]
+                .map(JSON.parse);
+        }
+        
+        /**
+         * 插件全部使用，数据
+         */
+        
+        let temp3 = temp.filter(e => e.optionsRadios == '1');
+
+        /**
+         * 插件条件使用，数据
+         */
+        
+        let temp2 = temp.filter(e => e.optionsRadios == '0' && (appVer == e.appVer || androidVer == e.systemVer || channl == e.channl));
+
+        /**
+         * 以下为分别获取最大值
+         */
+
+        if (temp3.length > 0) {
+            d1 = temp3.map(e => (e.plugVersion = Number(e.plugVersion), e)).reduce((pre, cur) => pre.plugVersion > cur.plugVersion ? pre : cur);
+        }
+        
+        if (temp2.length > 0) {
+            d2 = temp2.map(e => (e.plugVersion = Number(e.plugVersion), e)).reduce((pre, cur) => pre.plugVersion > cur.plugVersion ? pre : cur);
+        }
+
+        /**
+         * 以下为最终的最大值
+         */
+
+        d = ((d2 && d2.plugVersion) > (d1 && d1.plugVersion)) ? d2 : d1;
+
+        if (d && typeof data[i] == 'string') {
+
+            /**
+             * 数组里面是单数组的时候
+             */
+
+            arr = [];
+        }
+        arr.push(d);
     }
-    
-    if (temp2.length > 0) {
-        d2 = temp2.map(e => (e.plugVersion = Number(e.plugVersion), e)).reduce((pre, cur) => pre.plugVersion > cur.plugVersion ? pre : cur);
-    }
 
-    /**
-     * 以下为最终的最大值
-     */    
+    return arr.map(d => {
 
-    d = ((d2 && d2.plugVersion) > (d1 && d1.plugVersion)) ? d2 : d1;
-
-    if (d) {
         return {
             version: d.plugVersion,
             name: d.plugName.split('_')[0],
@@ -125,9 +179,7 @@ const objDate = async (ctx, data) => {
             isAll: Boolean(Number(d.optionsRadios)),
             path: d.path ? 'http://' + (host || (eth0[0].address + ':3002')) + d.path + '/' + d.name + '/' + d.plugName : '没有地址'
         };
-    } else {
-        return {};
-    }
+    }); 
 };
 
 module.exports = andirApiController;
